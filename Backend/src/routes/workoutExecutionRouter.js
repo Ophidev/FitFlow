e4a3aad4baa85427dc2449220f858d5b6e119e1f
ophidev <ophid7@gmail.com>
2026-02-19 12:15:38 +0530

    feat: created workoutExecutionRouter added a endpint /workout/start and implement resume & auto-skip logic for workout sessions
    - created workoutExecutionRouter and added a api endpoint /workout/start and started writing logic
    - Implemented workout session start logic with ownership validation
    - Added resume capability for same-day in_progress workouts
    - Introduced auto-skip mechanism for abandoned previous-day sessions
    - Calculated totalDuration on skipped sessions
    - Standardized date handling for daily grouping
    - Ensured only one active workout session per user
    - Improved response structure with resume flag

---
+++ Backend/src/routes/workoutExecutionRouter.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const userAuth = require("../middlewares/auth.js");
+const WorkoutDays = require("../models/workoutDays.js");
+const WorkoutLog = require("../models/workoutLogs.js");
+const Exercises = require("../models/exercises.js");
+const SetLogs = require("../models/setLogs.js");
+
+const workoutExecutionRouter = express.Router();
+
+workoutExecutionRouter.post("/workout/start", userAuth, async (req,res) => {
+
+    try {
+
+        //data need
+        const loggedInUser = req.user; //current loggedInUser
+        const { workoutDayId } = req.body; // from frontend
+
+        // validate workout day ownership
+        const workoutDay = await WorkoutDays.findOne({
+            _id: workoutDayId,
+            userId: loggedInUser._id,
+        });
+        
+        if (!workoutDay) {
+            throw new Error ("!! Workout day not found !!!");
+        }
+
+        // prepare today's date
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const now = new Date(); //current date and time
+
+        // check for active workout
+        const activeWorkout = await WorkoutLog.findOne({
+            userId: loggedInUser._id,
+            status: "in_progress"
+        }).sort({ createdAt: -1});
+
+        // Active workout exists
+        if (activeWorkout) {
+
+            const activeDate = new Date(activeWorkout.date);
+            activeDate.setHours(0, 0, 0, 0);
+
+            // CASE A - same day -> resume
+            if (activeDate.getTime() === today.getTime()) {
+
+                const exercisesList = await Exercises.find({
+                    workoutDayId: activeWorkout.workoutDayId,
+                    userId: loggedInUser._id
+                });
+
+                const completedSets = await SetLogs.find({
+                    workoutLogId: activeWorkout._id,
+                    userId: loggedInUser._id
+                });
+
+                return res.status(200).json({
+                    message: "Resuming workout",
+                    workoutLog: activeWorkout,
+                    exercisesList,
+                    completedSets,
+                    resume: true
+                });
+            }
+
+            // CASE B - Old day -> auto skip
+            activeWorkout.status = "skipped";
+            activeWorkout.completedAt = now;
+            activeWorkout.totalDuration = 
+                Math.floor((now - activeWorkout.startedAt) / 1000);
+
+                await activeWorkout.save();
+        }
+
+        // fetch exeercises for new workout
+        const allExercises = await Exercises.find({
+            workoutDayId: workoutDayId,
+            userId: loggedInUser._id,
+        });
+
+        if (allExercises.length === 0) {
+            return res.status(400).send("!No Exercises found for workout day");
+        }
+
+        const workoutLogInstance = new WorkoutLog({
+            userId: loggedInUser._id,
+            workoutDayId,
+            date: today,
+            startedAt: now,
+            totalExercises: allExercises.length,
+            totalSetsCompleted: 0,
+            status: "in_progress"
+        });
+
+        const savedWorkoutLogInstance = await workoutLogInstance.save();
+
+        res.status(201).json({
+            "message": "Workout Started",
+            "workoutLog": savedWorkoutLogInstance,
+            "exercisesList": allExercises,
+            "resume": false
+        });
+
+    } catch(err) {
+        res.status(400).send("ERROR inside /workout/start : "+ err.message);
+    }
+
+});
+
+module.exports = workoutExecutionRouter;
